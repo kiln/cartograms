bf63912c745724307ea410f53a05452687e00db0
Robin Houston <robin.houston@gmail.com>
2012-06-18 23:51:21 +0100

    Population by county
    From the ONS mid-2010 population estimates.

---
+++ sql/maps/county_region_list.py
@@ -0,0 +1,332 @@
+english_counties = {
+  "Bedfordshire": [
+    ["E06000055", "Bedford"],
+    ["E06000056", "Central Bedfordshire"],
+    ["E06000032", "Luton"]
+  ],
+  "Berkshire": [
+    ["E06000036", "Bracknell Forest"],
+    ["E06000037", "West Berkshire"],
+    ["E06000038", "Reading"],
+    ["E06000039", "Slough"],
+    ["E06000040", "Windsor and Maidenhead"],
+    ["E06000041", "Wokingham"]
+  ],
+  "Buckinghamshire": [
+    ["E10000002", "Buckinghamshire County"],
+    ["E06000042", "Milton Keynes"]
+  ],
+  "Cambridgeshire": [
+    ["E10000003", "Cambridgeshire County"],
+    ["E06000031", "City of Peterborough"]
+  ],
+  "Cheshire": [
+    ["E06000049", "Cheshire East"],
+    ["E06000050", "Cheshire West and Chester"],
+    ["E06000006", "Halton"],
+    ["E06000007", "Warrington"]
+  ],
+  "City of Bristol": [["E06000023", "City of Bristol"]],
+  "Cornwall": [
+    ["E06000052", "Cornwall"],
+    ["E06000053", "Isles of Scilly"]
+  ],
+  "Cumbria": [["E10000006", "Cumbria County"]],
+  "Derbyshire": [
+    ["E10000007", "Derbyshire County"],
+    ["E06000015", "City of Derby"]
+  ],
+  "Devon": [
+    ["E10000008", "Devon County"],
+    ["E06000026", "City of Plymouth"],
+    ["E06000027", "Torbay"]
+  ],
+  "Dorset": [
+    ["E10000009", "Dorset County"],
+    ["E06000028", "Bournemouth"],
+    ["E06000029", "Poole"]
+  ],
+  "Durham": [
+    ["E06000047", "County Durham"],
+    ["E06000005", "Darlington"],
+    ["E06000001", "Hartlepool"],
+    
+    # /* and the twenty wards to the north of the Tees in Stockton-on-Tees */
+    # ["E05001527", "Billingham Central Ward"],
+    # ["E05001528", "Billingham East Ward"],
+    # ["E05001529", "Billingham North Ward"],
+    # ["E05001530", "Billingham South Ward"],
+    # ["E05001531", "Billingham West Ward"],
+    # ["E05001532", "Bishopsgarth and Elm Tree Ward"],
+    # ["E05001533", "Eaglescliffe Ward"],
+    # ["E05001534", "Fairfield Ward"],
+    # ["E05001535", "Grangefield Ward"],
+    # ["E05001536", "Hardwick Ward"],
+    # ["E05001537", "Hartburn Ward"],
+    # ["E05001541", "Newtown Ward"],
+    # ["E05001542", "Northern Parishes Ward"],
+    # ["E05001543", "Norton North Ward"],
+    # ["E05001544", "Norton South Ward"],
+    # ["E05001545", "Norton West Ward"],
+    # ["E05001546", "Parkfield and Oxbridge Ward"],
+    # ["E05001547", "Roseworth Ward"],
+    # ["E05001549", "Stockton Town Centre Ward"],
+    # ["E05001551", "Western Parishes Ward"],
+    ["E06000004", "Stockton-on-Tees"] # pretend all of Stockton is in Durham
+  ],
+  "The East Riding of Yorkshire": [
+    ["E06000011", "East Riding of Yorkshire"],
+    ["E06000010", "City of Kingston upon Hull"]
+  ],
+  "East Sussex": [
+    ["E10000011", "East Sussex County"],
+    ["E06000043", "The City of Brighton and Hove"]
+  ],
+  "Essex": [
+    ["E10000012", "Essex County"],
+    ["E06000033", "Southend-on-Sea"],
+    ["E06000034", "Thurrock"]
+  ],
+  "Gloucestershire": [
+    ["E10000013", "Gloucestershire County"],
+    ["E06000025", "South Gloucestershire"]
+  ],
+  "Hampshire": [
+    ["E10000014", "Hampshire County"],
+    ["E06000044", "City of Portsmouth"],
+    ["E06000045", "City of Southampton"]
+  ],
+  "Herefordshire": [["E06000019", "County of Herefordshire"]],
+  "Hertfordshire": [["E10000015", "Hertfordshire County"]],
+  "Isle of Wight": [["E06000046", "Isle of Wight"]],
+  "Kent": [
+    ["E10000016", "Kent County"],
+    ["E06000035", "Medway"]
+  ],
+  "Lancashire": [
+    ["E10000017", "Lancashire County"],
+    ["E06000008", "Blackburn with Darwen"],
+    ["E06000009", "Blackpool"]
+  ],
+  "Leicestershire": [
+    ["E10000018", "Leicestershire County"],
+    ["E06000016", "City of Leicester"]
+  ],
+  "Lincolnshire": [
+    ["E10000019", "Lincolnshire County"],
+    ["E06000013", "North Lincolnshire"],
+    ["E06000012", "North East Lincolnshire"]
+  ],
+  "Norfolk": [["E10000020", "Norfolk County"]],
+  "Northamptonshire": [["E10000021", "Northamptonshire County"]],
+  "North Yorkshire": [
+    ["E10000023", "North Yorkshire County"],
+    ["E06000002", "Middlesbrough"],
+    ["E06000003", "Redcar and Cleveland"],
+    ["E06000014", "York"],
+    
+    # /* and the six wards to the south of the Tees in Stockton-on-Tees */
+    # ["E05001538", "Ingleby Barwick East"],
+    # ["E05001539", "Ingleby Barwick West"],
+    # ["E05001540", "Mandale and Victoria"],
+    # ["E05001548", "Stainsby Hill"],
+    # ["E05001550", "Village"],
+    # ["E05001552", "Yarm"],
+  ],
+  "Northumberland": [["E06000048", "Northumberland"]],
+  "Nottinghamshire": [
+    ["E10000024", "Nottinghamshire"],
+    ["E06000018", "City of Nottingham"]
+  ],
+  "Oxfordshire": [["E10000025", "Oxfordshire County"]],
+  "Rutland": [["E06000017", "Rutland"]],
+  "Shropshire": [
+    ["E06000051", "Shropshire"],
+    ["E06000020", "Telford and Wrekin"]
+  ],
+  "Somerset": [
+    ["E10000027", "Somerset County"],
+    ["E06000022", "Bath and North East Somerset"],
+    ["E06000024", "North Somerset"]
+  ],
+  "Suffolk": [["E10000029", "Suffolk County"]],
+  "Surrey": [["E10000030", "Surrey County"]],
+  "Staffordshire": [
+    ["E10000028", "Staffordshire County"],
+    ["E06000021", "City of Stoke-on-Trent"]
+  ],
+  "Warwickshire": [["E10000031", "Warwickshire County"]],
+  "West Sussex": [["E10000032", "West Sussex County"]],
+  "Wiltshire": [
+    ["E06000054", "Wiltshire"],
+    ["E06000030", "Swindon"]
+  ],
+  "Worcestershire": [["E10000034", "Worcestershire County"]],
+  
+  "Greater Manchester": [
+    ["E08000001", "Bolton District"],
+    ["E08000002", "Bury District"],
+    ["E08000003", "Manchester District"],
+    ["E08000004", "Oldham District"],
+    ["E08000005", "Rochdale District"],
+    ["E08000006", "Salford District"],
+    ["E08000007", "Stockport District"],
+    ["E08000008", "Tameside District"],
+    ["E08000009", "Trafford District"],
+    ["E08000010", "Wigan District"]
+  ],
+  "Merseyside": [
+    ["E08000011", "Knowsley District"],
+    ["E08000012", "Liverpool District"],
+    ["E08000013", "St. Helens District"],
+    ["E08000014", "Sefton District"],
+    ["E08000015", "Wirral District"]
+  ],
+  "South Yorkshire": [
+    ["E08000016", "Barnsley District"],
+    ["E08000017", "Doncaster District"],
+    ["E08000018", "Rotherham District"],
+    ["E08000019", "Sheffield District"]
+  ],  
+  "Tyne and Wear": [
+    ["E08000020", "Gateshead District"],
+    ["E08000021", "Newcastle upon Tyne District"],
+    ["E08000022", "North Tyneside District"],
+    ["E08000023", "South Tyneside District"],
+    ["E08000024", "Sunderland District"]
+  ],
+  "West Midlands": [
+    ["E08000025", "Birmingham District"],
+    ["E08000026", "Coventry District"],
+    ["E08000027", "Dudley District"],
+    ["E08000028", "Sandwell District"],
+    ["E08000029", "Solihull District"],
+    ["E08000030", "Walsall District"],
+    ["E08000031", "City of Wolverhampton District"]
+  ],
+  "West Yorkshire": [
+    ["E08000032", "Bradford District"],
+    ["E08000033", "Calderdale District"],
+    ["E08000034", "Kirklees District"],
+    ["E08000035", "Leeds District"],
+    ["E08000036", "Wakefield District"]
+  ],
+  
+  
+  # "Greater London": [
+  #   ["E09000001", "City and County of the City of London"],
+  #   ["E09000002", "Barking and Dagenham London Boro"],
+  #   ["E09000003", "Barnet London Boro"],
+  #   ["E09000004", "Bexley London Boro"],
+  #   ["E09000005", "Brent London Boro"],
+  #   ["E09000006", "Bromley London Boro"],
+  #   ["E09000007", "Camden London Boro"],
+  #   ["E09000008", "Croydon London Boro"],
+  #   ["E09000009", "Ealing London Boro"],
+  #   ["E09000010", "Enfield London Boro"],
+  #   ["E09000011", "Greenwich London Boro"],
+  #   ["E09000012", "Hackney London Boro"],
+  #   ["E09000013", "Hammersmith and Fulham London Boro"],
+  #   ["E09000014", "Haringey London Boro"],
+  #   ["E09000015", "Harrow London Boro"],
+  #   ["E09000016", "Havering London Boro"],
+  #   ["E09000017", "Hillingdon London Boro"],
+  #   ["E09000018", "Hounslow London Boro"],
+  #   ["E09000019", "Islington London Boro"],
+  #   ["E09000020", "Kensington and Chelsea London Boro"],
+  #   ["E09000021", "Kingston upon Thames London Boro"],
+  #   ["E09000022", "Lambeth London Boro"],
+  #   ["E09000023", "Lewisham London Boro"],
+  #   ["E09000024", "Merton London Boro"],
+  #   ["E09000025", "Newham London Boro"],
+  #   ["E09000026", "Redbridge London Boro"],
+  #   ["E09000027", "Richmond upon Thames London Boro"],
+  #   ["E09000028", "Southwark London Boro"],
+  #   ["E09000029", "Sutton London Boro"],
+  #   ["E09000030", "Tower Hamlets London Boro"],
+  #   ["E09000031", "Waltham Forest London Boro"],
+  #   ["E09000032", "Wandsworth London Boro"],
+  #   ["E09000033", "City of Westminster London Boro"]
+  # ]
+}
+
+welsh_counties = {
+  "Gwynedd": [
+    ["W06000001", "Sir Ynys Mon", "Isle of Anglesey"],
+    ["W06000002", "Gwynedd", "Gwynedd"]
+  ],
+
+  "Clwyd": [
+    ["W06000003", "Conwy", "Conwy"],
+    ["W06000004", "Sir Ddinbych", "Denbighshire"],
+    ["W06000005", "Sir y Fflint", "Flintshire"],
+    ["W06000006", "Wrecsam", "Wrexham"]
+  ],
+
+  "Dyfed": [
+    ["W06000008", "Sir Ceredigion", "Ceredigion"],
+    ["W06000009", "Sir Benfro", "Pembrokeshire"],
+    ["W06000010", "Sir Gaerfyrddin", "Carmarthenshire"]
+  ],
+
+  "West Glamorgan": [
+    ["W06000011", "Abertawe", "Swansea"],
+    ["W06000012", "Castell-nedd Port Talbot", "Neath Port Talbot"]
+  ],
+
+  "South Glamorgan": [
+    ["W06000014", "Bro Morgannwg", "the Vale of Glamorgan"],
+    ["W06000015", "Caerdydd", "Cardiff"]
+  ],
+
+  "Gwent": [
+    ["W06000018", "Caerffili", "Caerphilly"],
+    ["W06000019", "Blaenau Gwent", "Blaenau Gwent"],
+    ["W06000020", "Tor-faen", "Torfaen"],
+    ["W06000021", "Sir Fynwy", "Monmouthshire"],
+    ["W06000022", "Casnewydd", "Newport"]
+  ],
+
+  "Powys": [["W06000023", "Powys", "Powys"]],
+
+  "Mid Glamorgan": [
+    ["W06000013", "Pen-y-bont ar Ogwr", "Bridgend"],
+    ["W06000016", "Rhondda Cynon Taf", "Rhondda Cynon Taf"],
+    ["W06000024", "Merthyr Tudful", "Merthyr Tydfil"]
+  ]
+}
+
+scottish_council_regions = {
+ "S12000042": "Dundee City",
+ "S12000008": "East Ayrshire",
+ "S12000009": "East Dunbartonshire",
+ "S12000011": "East Renfrewshire",
+ "S12000014": "Falkirk",
+ "S12000043": "Glasgow City",
+ "S12000018": "Inverclyde",
+ "S12000038": "Renfrewshire",
+ "S12000026": "Scottish Borders",
+ "S12000041": "Angus",
+ "S12000029": "South Lanarkshire",
+ "S12000030": "Stirling",
+ "S12000005": "Clackmannanshire",
+ "S12000019": "Midlothian",
+ "S12000044": "North Lanarkshire",
+ "S12000024": "Perth and Kinross",
+ "S12000039": "West Dunbartonshire",
+ "S12000040": "West Lothian",
+ "S12000017": "Highland",
+ "S12000020": "Moray",
+ "S12000023": "Orkney Islands",
+ "S12000013": "Na h-Eileanan an Iar",
+ "S12000035": "Argyll and Bute",
+ "S12000034": "Aberdeenshire",
+ "S12000015": "Fife",
+ "S12000033": "Aberdeen City",
+ "S12000036": "City of Edinburgh",
+ "S12000010": "East Lothian",
+ "S12000027": "Shetland Islands",
+ "S12000021": "North Ayrshire",
+ "S12000006": "Dumfries and Galloway",
+ "S12000028": "South Ayrshire",
+}
---
+++ sql/maps/population_data.py
@@ -0,0 +1,35 @@
+import csv
+import os
+import sys
+
+from county_region_list import (english_counties, welsh_counties, scottish_council_regions)
+
+data_dir = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        os.path.pardir, os.path.pardir,
+        "data"
+    )
+)
+
+population_by_region = {}
+name_by_region = {}
+with open(os.path.join(data_dir, "population-by-region.csv")) as f:
+    r = csv.reader(f)
+    header = r.next()
+    for row in r:
+        code, name, population = row
+        population_by_region[code] = int(population)
+
+
+w = csv.writer(sys.stdout)
+w.writerow(["County name", "Population"])
+for county, parts in english_counties.items() + welsh_counties.items():
+    population = sum([
+        population_by_region[part[0]]
+        for part in parts
+    ])
+    w.writerow([county, population])
+
+for code, name in scottish_council_regions.items():
+    w.writerow([name, population_by_region[code]])
