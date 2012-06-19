dcec3cb02937698ea29ccf6c40c40da43e4316ea
Robin Houston <robin.houston@gmail.com>
2012-06-19 22:51:37 +0100

    Allow database connection details to be specified

--- bin/delete-data.py
+++ bin/delete-data.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python
 
+import optparse
 import sys
 import psycopg2
 
-dataset_name = sys.argv[1]
+parser = optparse.OptionParser(usage="%prog [options] dataset")
+parser.add_option("", "--db-host",
+                action="store",
+                default="localhost",
+                help="database hostname (default %default)")
+parser.add_option("", "--db-name",
+                action="store",
+                help="database name")
+parser.add_option("", "--db-user",
+                action="store",
+                help="database username")
 
-db = psycopg2.connect("host=localhost")
+(options, args) = parser.parse_args()
+if len(args) != 1:
+    parser.error("Wrong number of arguments")
+dataset_name, = args
+
+db_connection_string = "host=" + options.db_host
+if options.db_name:
+    db_connection_string += " dbname=" + options.db_name
+if options.db_user:
+    db_connection_string += " user=" + options.db_user
+db = psycopg2.connect(db_connection_string)
 
 c = db.cursor()
 c.execute("""
