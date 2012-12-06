da90982bebcf4ce2c6eb56d196a1da8fe11a04e5
Robin Houston <robin.houston@gmail.com>
2012-12-06 14:34:21 +0000

    Various new options
    New options:
     - The --region option allows you to make a map of just one region;
     - The stroke width is now specified in pixels, rather than geographic units;
     - The --box option allows you to scale the map to fill a box as full as
       possible whilst retaining the aspect ratio.

--- bin/as-png.py
+++ bin/as-png.py
@@ -4,6 +4,7 @@ from __future__ import division
 
 import math
 import optparse
+import re
 import sys
 
 import cairo
@@ -31,6 +32,18 @@ class AsPNG(object):
         self.fill_colour = self._parse_colour(options.fill_colour)
         self.fill_colour_no_data = self._parse_colour(options.fill_colour_no_data)
         self.stroke_colour = self._parse_colour(options.stroke_colour)
+        self.background_colour = self._parse_colour(options.background_colour)
+        
+        if options.region:
+            bounds = self.region_bounds(options.region)
+            self.x_min, self.y_min, self.x_max, self.y_max = bounds
+        else:
+            self.x_min = self.m.x_min
+            self.x_max = self.m.x_max
+            self.y_min = self.m.y_min
+            self.y_max = self.m.y_max
+        
+        aspect_ratio = (self.x_max - self.x_min) / (self.y_max - self.y_min)
         
         # If width and height are both specified, use them
         if options.width and options.height:
@@ -41,13 +54,24 @@ class AsPNG(object):
         # of the map as defined in the database.
         elif options.width:
             self.output_width = options.width
-            ratio = self.m.height / self.m.width
-            self.output_height = int(round(options.width * ratio))
+            self.output_height = int(round(options.width / aspect_ratio))
         
         elif options.height:
             self.output_height = options.height
-            ratio = self.m.width / self.m.height
-            self.output_width = int(round(options.height * ratio))
+            self.output_width = int(round(options.height * aspect_ratio))
+        
+        elif options.box:
+            mo = re.match(r"^(\d+)x(\d+)$", options.box)
+            max_width, max_height = int(mo.group(1)), int(mo.group(2))
+            box_ratio = max_width / max_height
+            if box_ratio > aspect_ratio:
+                # The height is the limiting factor
+                self.output_height = max_height
+                self.output_width = int(round(max_height * aspect_ratio))
+            else:
+                # The width is the limiting factor
+                self.output_width = max_width
+                self.output_height = int(round(max_width / aspect_ratio))
         
         # If neither is specified, use the dimensions of the map
         # as defined in the database.
@@ -72,11 +96,38 @@ class AsPNG(object):
         r, g, b = int(colour_string[0:2], 16), int(colour_string[2:4], 16), int(colour_string[4:6], 16)
         return r/0xFF, g/0xFF, b/0xFF
 
+    def region_bounds(self, region_name):
+        c = self.db.cursor()
+        try:
+            c.execute("""
+                select ST_XMin(x.box), ST_YMin(x.box)
+                     , ST_XMax(x.box), ST_YMax(x.box)
+                from (
+                    select Box2D(ST_Transform(region.the_geom, %(srid)s)) as box
+                    from region
+                    where name = %(region_name)s
+                    and division_id = %(division_id)s
+                ) x
+            """, {
+                "srid": self.m.srid,
+                "division_id": self.m.division_id,
+                "region_name": region_name
+            })
+            return c.fetchone()
+        finally:
+            c.close()
+    
     def render_region_paths(self, slide):
         c = self.db.cursor()
         try:
+            params = {
+                "srid": self.m.srid,
+                "simplification": self.options.simplification,
+                "dataset_name": self.options.dataset,
+                "division_id": self.m.division_id,
+            }
             if self.options.dataset:
-                c.execute("""
+                sql = """
                     select region.name
                              , ST_AsEWKB(ST_Simplify(ST_Transform(region.the_geom, %(srid)s), %(simplification)s)) g
                              , exists(
@@ -87,24 +138,22 @@ class AsPNG(object):
                                     and data_value.region_id = region.id) has_data
                     from region
                     where region.division_id = %(division_id)s
-                """, {
-                    "srid": self.m.srid,
-                    "simplification": self.options.simplification,
-                    "dataset_name": self.options.dataset,
-                    "division_id": self.m.division_id
-                })
+                """
             else:
-                c.execute("""
+                sql = """
                     select region.name
                              , ST_AsEWKB(ST_Simplify(ST_Transform(region.the_geom, %(srid)s), %(simplification)s)) g
                              , true
                     from region
                     where region.division_id = %(division_id)s
-                """, {
-                    "srid": self.m.srid,
-                    "simplification": self.options.simplification,
-                    "division_id": self.m.division_id
-                })
+                """
+            
+            if self.options.region:
+                sql += "and region.name = %(region_name)s"
+                params["region_name"] = self.options.region
+            
+            c.execute(sql, params)
+            
             for iso2, g, has_data in c.fetchall():
                 fill_colour = self.fill_colour if has_data else self.fill_colour_no_data
                 p = shapely.wkb.loads(str(g))
@@ -134,6 +183,7 @@ class AsPNG(object):
                 self.c.fill_preserve()
             else:
                 self.c.fill()
+        
         if self.stroke_colour:
             self.c.set_source_rgb(*self.stroke_colour)
             self.c.stroke()
@@ -146,8 +196,8 @@ class AsPNG(object):
             if self.interpolator:
                 x, y = self.interpolator(x, y, slide)
             polygon_coords.append((
-                (x - self.m.x_min) * self.output_width / (self.m.x_max - self.m.x_min),
-                self.output_height - (y - self.m.y_min) * self.output_height / (self.m.y_max - self.m.y_min),
+                (x - self.x_min) * self.output_width / (self.x_max - self.x_min),
+                self.output_height - (y - self.y_min) * self.output_height / (self.y_max - self.y_min),
             ))
             
         self.draw.polygon(polygon_coords, outline=self.stroke_colour, fill=fill_colour)
@@ -202,20 +252,29 @@ class AsPNG(object):
             self.render_frame(1.0, self.out)
     
     def render_frame_cairo(self, slide, output_file):
-        surface = cairo.ImageSurface(cairo.FORMAT_RGB24, self.output_width, self.output_height)
+        surface = cairo.ImageSurface(
+            cairo.FORMAT_ARGB32,
+            self.output_width + self.options.stroke_width,
+            self.output_height + self.options.stroke_width,
+        )
         self.c = cairo.Context(surface)
-        
-        self.c.rectangle(0,0, self.output_width, self.output_height)
-        self.c.set_source_rgb(0x9e/0xff, 0xc7/0xff, 0xf3/0xff)
-        self.c.fill()
 
+        if self.background_colour:
+            self.c.rectangle(0,0, self.output_width, self.output_height)
+            self.c.set_source_rgb(*self.background_colour)
+            self.c.fill()
+
+        stroke_width = self.options.stroke_width
+        input_width = self.x_max - self.x_min
+        input_height = self.y_max - self.y_min
+        width_ratio = self.output_width / input_width
+        height_ratio = self.output_height / input_height
+        mean_ratio = (width_ratio + height_ratio) / 2
         self.c.transform(cairo.Matrix(
-            self.output_width / (self.m.x_max - self.m.x_min), 0,
-            0, - self.output_height / (self.m.y_max - self.m.y_min),
-            -self.m.x_min * self.output_width / (self.m.x_max - self.m.x_min),
-            self.m.y_max * self.output_height / (self.m.y_max - self.m.y_min),
+            width_ratio, 0, 0, -height_ratio,
+            -self.x_min * width_ratio + stroke_width/2, self.y_max * height_ratio + stroke_width/2,
         ))
-        self.c.set_line_width(self.options.stroke_width)
+        self.c.set_line_width(stroke_width / mean_ratio)
 
         self.render_region_paths(slide)
         if self.options.circles:
@@ -293,7 +352,7 @@ def main():
     parser.add_option("", "--circle-fill-colour",
                       action="store", default="FF0000",
                       help="fill colour for circles (default %default)")
-
+    
     parser.add_option("", "--cairo",
                       action="store_true", default=True,
                       help="use Cairo")
@@ -304,6 +363,9 @@ def main():
                       action="store",
                       help="overlay the paths on the specified background image")
     
+    parser.add_option("", "--background-colour",
+                      action="store", default="9EC7F3",
+                      help="background colour (default %default)")
     parser.add_option("", "--fill-colour",
                       action="store", default="F7D3AA",
                       help="fill colour (default %default)")
@@ -314,8 +376,8 @@ def main():
                       action="store", default="A08070",
                       help="stroke colour (default %default)")
     parser.add_option("", "--stroke-width",
-                      action="store", default=2000, type="int",
-                      help="width of strokes (default %default)")
+                      action="store", default=2, type="int",
+                      help="width of strokes, in pixels (default %default)")
     
     parser.add_option("", "--width",
                       action="store", type="int",
@@ -323,6 +385,13 @@ def main():
     parser.add_option("", "--height",
                       action="store", type="int",
                       help="height of output image")
+    parser.add_option("", "--box",
+                      action="store",
+                      help="fit image to box, e.g. 200x200")
+    
+    parser.add_option("", "--region",
+                      action="store",
+                      help="map just the specified region")
     
     (options, args) = parser.parse_args()
     if args:
@@ -340,6 +409,14 @@ def main():
             options.output % (0,)
         except:
             parser.error("Output filename '%s' does not contain a %%d template" % (options.output,))
+    
+    if options.box:
+        if options.width:
+            parser.error("You can't specify --box and --width")
+        if options.height:
+            parser.error("You can't specify --box and --height")
+        if not re.match(r"^\d+x\d+$", options.box):
+            parser.error("Failed to parse --box value: "+ options.box)
     
     if options.overlay_on and options.cairo:
         parser.error("The --overlay-on option is only allowed in --pil mode")
