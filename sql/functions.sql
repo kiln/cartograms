kiln/cartograms

commit 89724dbf1aa4487ae7c999dc34cea53bc5b67ab2

Only generate grid points within the projection region

sql/functions.sql
@@ -28,6 +28,30 @@ begin
                ), 4326
              ) pt_4326
       from pregrid
+      -- NB: This is only correct for pseudocylindrical projections!
+      where x
+        between ST_X(ST_Transform(
+            ST_SetSRID(
+                ST_MakePoint(
+                    -180,
+                    ST_Y(
+                        ST_Transform(ST_SetSRID(ST_MakePoint(0, my), srid), 4326)
+                    )
+                ),
+                4326
+            ), srid
+        ))
+        and ST_X(ST_Transform(
+            ST_SetSRID(
+                ST_MakePoint(
+                    +180,
+                    ST_Y(
+                        ST_Transform(ST_SetSRID(ST_MakePoint(0, my), srid), 4326)
+                    )
+                ),
+                4326
+            ), srid
+        ))
   );
 end;
 $$ language 'plpgsql';

commit 78cf91aa47bf4d3113340a3aa5f707bd5fc14b07

Fix populate_grid for OSGB1936
It looks as though we broke it in 89724db.

sql/functions.sql
@@ -29,7 +29,15 @@ begin
              ) pt_4326
       from pregrid
       -- NB: This is only correct for pseudocylindrical projections!
-      where mx
+      --
+      -- The point is that, if we are making a map of the whole world on a
+      -- projection where the image of the world is not a rectangle, there
+      -- will be some grid points that lie outside the world. These may
+      -- cause errors or other infelicities, so we would like to exclude
+      -- them. The logic below works for pseudocylindrical projections only.
+      -- It will cause an error for regional projections such as OSGB1936
+      -- (srid 27700), so for now we just exclude them explicitly.
+      where srid=27700 or mx
         between ST_X(ST_Transform(
             ST_SetSRID(
                 ST_MakePoint(